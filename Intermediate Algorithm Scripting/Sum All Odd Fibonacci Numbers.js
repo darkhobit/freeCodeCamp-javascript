cb3607e29996e3641a251afd76596e16822d5e67
darkhobit <120219361+darkhobit@users.noreply.github.com>
2023-08-19 12:27:17 -0400

    Update Sum All Odd Fibonacci Numbers.js

--- Intermediate Algorithm Scripting/Sum All Odd Fibonacci Numbers.js
+++ Intermediate Algorithm Scripting/Sum All Odd Fibonacci Numbers.js
@@ -1,26 +1,40 @@
 //https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/sum-all-odd-fibonacci-numbers
 
-//I  have not checked this code yet
+//this code was made with the help of ChatGPT, because the first one I made produced the message: "Potential infinite loop detected"
+//the code with PROBLEM is at the botton
+function sumFibs(num) {
+  
+  let fibArr = [1, 1]; // Initialize the array with the first two Fibonacci numbers
+  let sumOdd = 0;
+
+  if(num <= 2) {
+    sumOdd = num;
+  } else {
+    sumOdd = 2;
+    for (let i = 2; i <= num; i++) {
+      fibArr[i] = fibArr[i - 1] + fibArr[i - 2];
+      if (fibArr[i] % 2 === 1 && fibArr[i] <= num) { //sum off odd Fibonacci numbers less than or equal to "num"
+        sumOdd += fibArr[i];
+      }
+    }
+  }
+  return sumOdd;
+}
+
+let result = sumFibs(75024);
+console.log(result);
+
+
+/*
 function sumFibs(num) {
  
   let sumOdd = 0;
-  for(let i = 0; i <= num; i++) {
+  for(let i = 0; i <= num; i++) {  //the WARNING message was for this line
     if(fibonacci(i) % 2 === 1 && fibonacci(i) < num) {
       sumOdd += fibonacci(i);
-      console.log(fibonacci(i))
+      //console.log(fibonacci(i))
     }
   }
     return sumOdd;
 }
-
-function fibonacci(num) {
-    if(num < 2) {
-        return num;
-    }
-    else {
-        return fibonacci(num-1) + fibonacci(num - 2);
-    }
-}
-
-let result = sumFibs(20);
-console.log(result);
+*/
