69ca2740a548bce9ca94c9e5d95b0b02193481d3
darkhobit <120219361+darkhobit@users.noreply.github.com>
2023-08-18 22:20:14 -0400

    Update Search and Replace.js

--- Intermediate Algorithm Scripting/Search and Replace.js
+++ Intermediate Algorithm Scripting/Search and Replace.js
@@ -1,10 +1,15 @@
 //https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/search-and-replace
 
 function myReplace(str, before, after) {
-//pending to check if capital letter in 'before' match with 'after'
-  return str.replace(before, after);
-  
+  let upperAfter = after.slice();
+
+  if(before.charAt(0) === before.charAt(0).toUpperCase()) {
+    upperAfter = after.charAt(0).toUpperCase() + after.slice(1);
+  } else {
+    upperAfter = after.charAt(0).toLowerCase() + after.slice(1);
+  }
+  return str.replace(before, upperAfter);
 }
 
-let result = myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
+let result = myReplace("I think we should look up there", "up", "Down");
 console.log(result);
