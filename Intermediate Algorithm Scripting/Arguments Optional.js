d0c8024b96074fa5f9ca0f91a54fe39d132ab8e6
darkhobit <120219361+darkhobit@users.noreply.github.com>
2023-08-20 20:30:29 -0400

    Update Arguments Optional.js

--- Intermediate Algorithm Scripting/Arguments Optional.js
+++ Intermediate Algorithm Scripting/Arguments Optional.js
@@ -1,15 +1,31 @@
 //https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/arguments-optional
-//I need to finish this exercise
+
 function addTogether() {
   let arg = [...arguments];
-  if(arg.length == 2) {
-    return arg[0] + arg[1];
-  } else {
-
-  }
+/*  this could be one way to check if all arguments are numbers, but the other way is simpler
+  let bol = true;
+  arg.forEach(item => {
+    if(typeof item !== "number") {
+      bol = false;
+    } 
+  });
+*/
+  const bol = arg.every(item => typeof item === "number"); //a simpler way to check
   
-  return false;
+  if(arg.length == 2 && bol) {
+    return arg[0] + arg[1];
+  } else if(arg.length == 1 && bol) {
+    return function() {
+      let arg2 = [...arguments];
+      const bol2 = arg2.every(item => typeof item === "number");
+      if(bol2) {
+        return arg[0] + arg2[0];
+      }       
+    }
+  } else {
+    return undefined;
+  }
 }
 
-let result = addTogether(2,3);
+let result = addTogether(2)(7);
 console.log(result);
