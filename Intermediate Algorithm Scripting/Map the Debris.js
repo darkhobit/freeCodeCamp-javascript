eff1c2b12f1e4e6321f5ad485abee6d5671923f3
darkhobit <120219361+darkhobit@users.noreply.github.com>
2023-08-21 13:15:36 -0400

    Update Map the Debris.js

--- Intermediate Algorithm Scripting/Map the Debris.js
+++ Intermediate Algorithm Scripting/Map the Debris.js
@@ -31,3 +31,25 @@ function orbitalP(avg) {
 
 let result = orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
 console.log(result);
+
+
+//****************ANOTHER SOLUTION (I copied this and made slight changes)************
+
+function orbitalPeriod(arr) {
+  const GM = 398600.4418;
+  const earthRadius = 6367.4447;
+  const newArr = [];
+  
+  for (let elem in arr) {
+    const orbitalPer = Math.round(
+       2 * Math.PI * Math.sqrt(Math.pow(Object.values(arr[elem])[1] + earthRadius, 3) / GM)  //I wanted to use Object.keys() and Object.values() to don't need to know the property name
+    );
+    newArr.push({[Object.keys(arr[elem])[0]]: Object.values(arr[elem])[0], orbitalPeriod: orbitalPer});
+  }
+
+  return newArr;
+}
+
+// test here
+let result = orbitalPeriod([{ name: "sputnik", avgAlt: 35873.5553 }]);
+console.log(result);
