1bc30d4ed0b9fa5831b083c8acbbb901e86d26d0
darkhobit <120219361+darkhobit@users.noreply.github.com>
2023-08-22 17:00:36 -0400

    Update Caesars Cipher.js

--- JavaScript Algorithms and Data Structures Projects/Caesars Cipher.js
+++ JavaScript Algorithms and Data Structures Projects/Caesars Cipher.js
@@ -1,5 +1,23 @@
 //https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/caesars-cipher
 
-//Let's see how I do
+function rot13(str) {
+  let outStr =  str.split('');
+  let inChar;
+  let outChar;
 
-//working....
+  for(let i = 0; i < outStr.length; i++) {
+    if(/[A-Z]/.test(outStr[i])) {
+      inChar = outStr[i].charCodeAt()
+      if(inChar + 13 > 90) {
+        inChar -= 26;
+      }
+      outChar = String.fromCharCode(inChar + 13)
+      outStr.splice(i, 1, outChar)
+    }
+  }
+  
+  return outStr.join('');
+}
+
+let result = rot13("SERR PBQR PNZC");
+console.log(result);
