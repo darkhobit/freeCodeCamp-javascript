b3090a48f933fa3364dc799c47e751f1b10df13c
darkhobit <120219361+darkhobit@users.noreply.github.com>
2023-08-22 12:30:02 -0400

    Update Palindrome Checker.js

--- JavaScript Algorithms and Data Structures Projects/Palindrome Checker.js
+++ JavaScript Algorithms and Data Structures Projects/Palindrome Checker.js
@@ -1,3 +1,29 @@
 //https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/palindrome-checker
 
-//I will do it soon. I hope!
+function palindrome(str) {
+  //variant 1 of filtering
+  /*let filterStr = str.split(/[^a-zA-Z]/)
+  .join('')
+  .toLowerCase()
+  .split(''); */
+  
+  //variant 2 of filtering
+  let newStr = str.toLowerCase().split('')
+  .filter(item => /[a-z0-9]/.test(item));
+  let len = newStr.length;
+  let bol;
+
+  for(let i = 0; i < len/2; i++) {
+    if(newStr[i] === newStr[(len - 1) - i]) {   
+      bol = true;
+    } else {
+      bol = false;
+      break;
+    }
+  }
+
+  return bol;
+}
+
+let result = palindrome("almostomla");
+console.log(result);
