9025bbb35c0c6159d2a16aa9e09b470b1c7d1391
darkhobit <120219361+darkhobit@users.noreply.github.com>
2023-08-22 15:33:17 -0400

    Create Roman Numeral Converter.js

---
+++ JavaScript Algorithms and Data Structures Projects/Roman Numeral Converter.js
@@ -0,0 +1,30 @@
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/roman-numeral-converter
+
+function convertToRoman(num) {
+ 
+  if(num < 3999  && num > 0) {
+    const romans = [
+    ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'],
+    ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'],
+    ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'],
+    ['', 'M', 'MM', 'MMM']
+    ];
+    let numArr = num.toString().split('').map(digit => parseInt(digit));
+    let len = numArr.length;
+    let romNum = [];
+    let count = 0;
+   
+    for(let i = len - 1; i >= 0; i--) {
+       romNum.unshift(romans[count][numArr[i]]);
+       count++;
+    };
+
+  } else {
+    return "The number must be between 0 and 3999";
+  };
+
+ return romNum.join('');
+ }
+
+let result = convertToRoman(-5000);
+console.log(result);
