4bf33bf53cb4eff518f59bb9fb9acac7865e66db
darkhobit <120219361+darkhobit@users.noreply.github.com>
2023-08-23 14:17:39 -0400

    Update Cash Register.js

--- JavaScript Algorithms and Data Structures Projects/Cash Register.js
+++ JavaScript Algorithms and Data Structures Projects/Cash Register.js
@@ -58,6 +58,6 @@ function checkCashRegister(price, cash, cid) {
     
   return change;
 }
-
+//test here
 let result = checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
 console.log(result);
